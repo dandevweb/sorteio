0e388f92e32d32df91c0cba026b4a70dc27c4054
Danilo Augusto <dandevweb@gmail.com>
2023-07-26 20:19:39 -0300

    initial commit

---
+++ app/Livewire/Sorteio.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Candidate;
+use Illuminate\View\View;
+use Livewire\Component;
+
+class Sorteio extends Component
+{
+    public $winner = null;
+
+    public function render(): View
+    {
+        return view('livewire.sorteio');
+    }
+
+    public function run(): void
+    {
+        $winner = Candidate::query()->inRandomOrder()->first();
+        $this->winner = $winner->name;
+    }
+}
